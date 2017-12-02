e04231138287341090a325b96bdfa0c031e40021
Ken Vilar <kenvilar@gmail.com>
2017-12-03 01:04:39 +0800

    Create an eventListener for button roll-dice and insert its anonymous function that will generate number and display the two dice images

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0]; // Player 1 and Player 2
 roundScore = 0; // Initial Score for round score
 activePlayer = 1; // Current active player
 
-dice = Math.floor((Math.random() * 6) + 1);
-
-// document.querySelector('#current-' + activePlayer).textContent = dice; //Select the element
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
 document.querySelector('#dice-1').style.display = 'none';
 document.querySelector('#dice-2').style.display = 'none';
+
+document.querySelector('.btn-roll-dice').addEventListener('click', function () {
+	/* TODO */
+	//Random number
+	var dice = Math.floor((Math.random() * 6) + 1);
+	//Display result
+	document.querySelector('#dice-1').style.display = 'block';
+	document.querySelector('#dice-2').style.display = 'block';
+	document.querySelector('#dice-1').src = 'dice' + dice + '.png';
+	document.querySelector('#dice-2').src = 'dice' + dice + '.png';
+});
+
+
+///////
+// document.querySelector('#current-' + activePlayer).textContent = dice; //Select the element
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
