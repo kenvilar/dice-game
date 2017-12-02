2a52c4162317cd126bfe5c58c729fd66b3a1c038
Ken Vilar <kenvilar@gmail.com>
2017-12-02 23:20:30 +0800

    Update style for player current box

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var score1 = 0; // Player 1
-var score2 = 0; // Player 2
-var roundScore = 0; // Initial Score for round score
-var activePlayer = 0; // Who are the current active player
+var scores, roundScore, activePlayer;
+
+scores = [0, 0]; // Player 1 and Player 2
+roundScore = 0; // Initial Score for round score
+activePlayer = 0; // Who are the current active player
