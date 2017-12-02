998cb2eaad11515f84ba8344a36877ad2735e98a
Ken Vilar <kenvilar@gmail.com>
2017-12-03 00:04:15 +0800

    Fix the random generator and convert it to Math.floor()

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ scores = [0, 0]; // Player 1 and Player 2
 roundScore = 0; // Initial Score for round score
 activePlayer = 0; // Current active player
 
-dice = Math.random(Math.floor() * 6);
+dice = Math.floor((Math.random() * 6) + 1);
 
 document.querySelector('#current-1').textContent = dice; //Select the element
