0b241835734f4403f643162a058984d5fa175a2c
Ken Vilar <kenvilar@gmail.com>
2017-12-03 00:25:34 +0800

    Change the plaintext textContent and replace with custom html element innerHTML

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0]; // Player 1 and Player 2
 roundScore = 0; // Initial Score for round score
-activePlayer = 0; // Current active player
+activePlayer = 1; // Current active player
 
 dice = Math.floor((Math.random() * 6) + 1);
 
-document.querySelector('#current-1').textContent = dice; //Select the element
+// document.querySelector('#current-' + activePlayer).textContent = dice; //Select the element
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
