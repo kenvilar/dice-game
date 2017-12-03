4f92e278fd9647017f8aac4acba3f974f2a631d7
Ken Vilar <kenvilar@gmail.com>
2017-12-03 18:51:35 +0800

    Hide also the second dice when winner exists

--- main.js
+++ main.js
@@ -42,7 +42,8 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 			//id="name-1"
 			document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
 
-			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('#dice-1').style.display = 'none';
+			document.querySelector('#dice-2').style.display = 'none';
 
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
