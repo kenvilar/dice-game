a0e1a1e6a1c61b2a0974889365765f1d656450b0
Ken Vilar <kenvilar@gmail.com>
2017-12-03 17:40:10 +0800

    If winner exists then set game to inactive else active; Initialize isGameActive to true;

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
-var scores, rolledScore, activePlayer;
+var scores, rolledScore, activePlayer, isGameActive;
 
 reset();
 
 document.querySelector('.btn-roll-dice').addEventListener('click', function () {
-	//Random number
-	var dice = Math.floor((Math.random() * 6) + 1);
-	//Display result
-	var diceObj1 = document.querySelector('#dice-1');
-	var diceObj2 = document.querySelector('#dice-2');
-	diceObj1.style.display = 'block';
-	diceObj2.style.display = 'block';
-	diceObj1.src = 'dice' + dice + '.png';
-	diceObj2.src = 'dice' + dice + '.png';
-	/* TODO */
-	//Update round score if the rolled number is not 1
-	if (dice !== 1) {
-		//Add score
-		rolledScore = rolledScore + dice;
-		document.querySelector('#current-' + activePlayer).textContent = rolledScore;
-	} else {
-		nextPlayer();
+	if (isGameActive) {
+		//Random number
+		var dice = Math.floor((Math.random() * 6) + 1);
+		//Display result
+		var diceObj1 = document.querySelector('#dice-1');
+		var diceObj2 = document.querySelector('#dice-2');
+		diceObj1.style.display = 'block';
+		diceObj2.style.display = 'block';
+		diceObj1.src = 'dice' + dice + '.png';
+		diceObj2.src = 'dice' + dice + '.png';
+		/* TODO */
+		//Update round score if the rolled number is not 1
+		if (dice !== 1) {
+			//Add score
+			rolledScore = rolledScore + dice;
+			document.querySelector('#current-' + activePlayer).textContent = rolledScore;
+		} else {
+			nextPlayer();
+		}
 	}
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
-	//Add rolled score to the player main score
-	scores[activePlayer - 1] = scores[activePlayer - 1] + rolledScore;
+	if (isGameActive) {
+		//Add rolled score to the player main score
+		scores[activePlayer - 1] = scores[activePlayer - 1] + rolledScore;
 
-	//Update and display the player main score
-	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer - 1];
+		//Update and display the player main score
+		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer - 1];
 
-	//Check if the player won the game
-	if (scores[activePlayer - 1] >= 10) {
-		//id="name-1"
-		document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+		//Check if the player won the game
+		if (scores[activePlayer - 1] >= 10) {
+			//id="name-1"
+			document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
 
-		document.querySelector('#dice-1').style.display = 'none';
-		document.querySelector('#dice-2').style.display = 'none';
+			document.querySelector('#dice-1').style.display = 'none';
+			document.querySelector('#dice-2').style.display = 'none';
 
-		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-	} else {
-		nextPlayer();
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+			isGameActive = false;
+		} else {
+			nextPlayer();
+		}
 	}
 });
 
@@ -66,6 +72,8 @@ function reset() {
 	scores = [0, 0];
 	rolledScore = 0;
 	activePlayer = 1;
+
+	isGameActive = true;
 
 	document.querySelector('#name-1').textContent = 'PLAYER 1';
 	document.querySelector('#name-2').textContent = 'PLAYER 2';
